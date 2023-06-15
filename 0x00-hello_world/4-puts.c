9fd5c0563e10668c0ed60b1776ca401ac2068c81
Mamabolo Katlego <rabotaikatlego@gmail.com>
2023-06-15 08:32:18 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
+
 /**
  * main - prints "programming is like building a multilingual
- * puzzle. follwed by a newline."
+ * puzzle, follwed by a newline."
  * return 0.
  */
 
 int main(void)
 
 {
-        puts("\"programming is like building a mutlilingual puzzle);
+        puts("\"programming is like building a mutlilingual puzzle");
 
         return(0);
 }
