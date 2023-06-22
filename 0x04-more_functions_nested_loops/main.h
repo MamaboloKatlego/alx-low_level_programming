2abb693f4f6ca75bab0b806d7b3d18c26812c016
Mamabolo Katlego <rabotaikatlego@gmail.com>
2023-06-22 15:52:15 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -14,10 +14,10 @@ void print_line(int n);
 
 void print_diagonal(int n);
 
-void print_square(int size);
-
 void print_triangle(int size);
 
 void print_number(int n);
 
 int _putchar(char c);
+
+void print_square(int size);
