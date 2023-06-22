0306924a216bf425357aa68a8c6b3a5e5853ffe2
Mamabolo Katlego <rabotaikatlego@gmail.com>
2023-06-22 02:22:17 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,15 +2,15 @@
 
 /**
  * _isupper - Check if a letter is upper
- * @n: The number to be checked
+ * @c: The number to be checked
  * Return: 1 for upper letter or 0 for any else
  */
 
-int _isupper(int n)
+int _isupper(int c)
 
 {
 
-	if (n >= 65 && n <= 98)
+	if (c >= 65 && c <= 98)
 	{
 	return (1);
 	}
