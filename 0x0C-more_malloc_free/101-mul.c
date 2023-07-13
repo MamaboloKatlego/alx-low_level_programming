907ab0de39b5800948014eba5dbe88635a3d893f
Mamabolo Katlego <rabotaikatlego@gmail.com>
2023-07-13 14:02:03 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,6 @@
-#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "main.h"
 
 /**
  * is_digit - checks if a string contains a non-digit char
@@ -72,7 +72,7 @@ int main(int argc, char *argv[])
 		result[i] = 0;
 	for (len1 = len1 - 1; len >= 0; len1--)
 	{
-		digit = s1[len1] - '0';
+		digit1 = s1[len1] - '0';
 		carry = 0;
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
