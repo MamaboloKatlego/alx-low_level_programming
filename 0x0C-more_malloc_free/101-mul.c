285c268c0fc93aafc3462cde470081c361136666
Mamabolo Katlego <rabotaikatlego@gmail.com>
2023-07-13 14:15:05 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,8 @@
+#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
+
+#define ERR_MSG "Error"
 
 /**
  * is_digit - checks if a string contains a non-digit char
@@ -22,7 +24,7 @@ int is_digit(char *s)
 }
 
 /**
- * _strlen - returns the length of a srting
+ * _strlen - returns the length of a string
  * @s: string to evaluate
  *
  * Return: the length of the string
@@ -61,7 +63,7 @@ int main(int argc, char *argv[])
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-		errore();
+		errors();
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
@@ -70,7 +72,7 @@ int main(int argc, char *argv[])
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
-	for (len1 = len1 - 1; len >= 0; len1--)
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
@@ -92,7 +94,7 @@ int main(int argc, char *argv[])
 			_putchar(result[i] + '0');
 	}
 	if (!a)
-		_putcar('0');
+		_putchar('0');
 	_putchar('\n');
 	free(result);
 	return (0);
