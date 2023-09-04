86a44a3a2251f475cdef9f634e9d007d15aca7a7
Mamabolo Katlego <rabotaikatlego@gmail.com>
2023-09-04 04:13:32 -0700

    task 1 and 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,14 +19,14 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];
-		 len++;
+		len++;
 	}
 
 	o = open(filename, O_WRONLY | O_APPEND);
 	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
-	return (-1);
+		return (-1);
 
 	close(o);
 
