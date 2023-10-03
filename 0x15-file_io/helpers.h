eb440c9b76871f71acee1ae7a0a7f0474b14877b
MamaboloKatlego <Rabotaikatlego@gmail.com>
2023-10-02 18:53:54 -0700

    100-elf_header.c

--- 0x15-file_io/helpers.h
+++ 0x15-file_io/helpers.h
@@ -1,5 +1,11 @@
+#ifndef
+#define
+
 void print_abi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 int main(int __attribute__((__unused__)) argc, char *argv[]);
 void close_elf(int elf);
+void check_elf(unsigned char *e_ident);
+
+#endif
