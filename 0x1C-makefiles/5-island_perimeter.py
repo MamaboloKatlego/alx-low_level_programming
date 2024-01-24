4923dc45810e3c395f56cb6b0489a966c5a8ead5
MamaboloKatlego <rabotaikatlego@gmail.com>
2024-01-24 01:16:19 -0800

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """
-File: 5-island_perimeter.py
-
 Author: Mamabolo Katlego  <rabotaikatlego@gmail.com>
 
-Function that returns the perimeter of the island described in grid
+A Function that returns the perimeter of the island described in grid.
 """
 
 
@@ -28,12 +26,12 @@ def island_perimeter(grid):
     """
     size = 0
     edges = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == 1:
+    for a in range(len(grid)):
+        for b in range(len(grid[0])):
+            if grid[a][b] == 1:
                 size += 1
-                if (j > 0 and grid[i][j - 1] == 1):
+                if (b > 0 and grid[a][b - 1] == 1):
                     edges += 1
-                if (j > 0 and grid[i - 1][j] == 1):
+                if (b > 0 and grid[i - 1][b] == 1):
                     edges += 1
     return (size * 4 - edges * 2)
